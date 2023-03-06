82b511fcfab8741c78b18ae6b6089f2eaef2b5a1
saajida mathen <mathensaajida@gmail.com>
2023-03-06 18:17:29 +0200

    fixed index view error

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Item.create(item_name: "car", description: "broken", price: 1000, user_id: 1)
