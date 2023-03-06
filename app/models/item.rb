6c3b5839477e33a014b932373685c1ce922ac7ef
Jonathan Watkins <jonrw97@gmail.com>
2023-03-06 20:47:46 +0200

    changed form to simple form/fixed create post

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
 
-  # validates :item_name, presence: true
-  # validates :description, presence: true
-  # validates :price, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
