6ca37a017596265762ec4e32a6138e0f9ed07f3f
Jonathan Watkins <jonrw97@gmail.com>
2023-03-08 11:05:47 +0200

    added validators to phot and descrtion of items

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :receipt
   validates :item_name, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
   validates :price, presence: true
   has_one_attached :photo
+  validates :photo, presence: true
 end
