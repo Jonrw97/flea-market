02e36164bd35ad3eabef86cc972172e47b4ddfb8
Jonathan Watkins <jonrw97@gmail.com>
2023-03-09 13:04:18 +0200

    feat geocoder init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_135457) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_104313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_135457) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
