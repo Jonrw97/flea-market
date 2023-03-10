c4e074f21335fe8aea62c74f790d9ee4147f7e1a
Jonathan Watkins <jonrw97@gmail.com>
2023-03-10 15:28:57 +0200

    adding to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ p 'Creating users'
 user1 = User.create(
   first_name: "Campbell",
   last_name: "Davies-Webb",
-  address: "Bergen, Norway",
+  address: "Hvam, Norway",
   email: "camie@gmail.com",
   password: 'test123'
 )
@@ -58,7 +58,7 @@ p "Created item id: #{item2.id}"
 user2 = User.create(
   first_name: "Jonathan",
   last_name: "Watkins",
-  address: "Tromso, Norway",
+  address: "Sandvika, Norway",
   email: "jon@gmail.com",
   password: 'test123'
 )
@@ -98,7 +98,7 @@ p "Created item id: #{item4.id}"
 user3 = User.create(
   first_name: "Saaj",
   last_name: "Mathen",
-  address: "Oslo, Norway",
+  address: "ski, Norway",
   email: "saaj@gmail.com",
   password: 'test123'
 )
@@ -138,7 +138,7 @@ p "Created item id: #{item6.id}"
 user4 = User.create(
   first_name: "Joshua",
   last_name: "Singrew",
-  address: "Trondheim, Norway",
+  address: "Fosser, Norway",
   email: "topshagger@gmail.com",
   password: 'test123'
 )
